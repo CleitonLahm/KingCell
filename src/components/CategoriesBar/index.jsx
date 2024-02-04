d990adc52767c0e5dda5821dc8858e5613fb3fec
cleiton lahm <cleijllahm@gmail.com>
2024-02-04 15:23:14 -0300

    finalizando a barra de categorias

--- src/components/CategoriesBar/index.jsx
+++ src/components/CategoriesBar/index.jsx
@@ -1,24 +1,43 @@
 import React from 'react'
-import { List } from './styles'
+import { List, ListLink, CategoriesContainer } from './styles'
+
+import iconOffer from '../../assets/icon-offer.gif'
+import iconPc from '../../assets/icon-pc.png'
+import iconSmartphone from '../../assets/icon-smartphone.png'
+import iconSound from '../../assets/icon-sound.png'
 
 const CategoriesBar = () => {
   return (
-    <div className='container'>
+    <CategoriesContainer>
+      <div className='container'>
       <List>
         <li>
-          <a href="#">Todas as categorias</a>
+          <ListLink href="#">
+            <img src={iconOffer} alt="icone de oferta" />
+            Ofertas
+          </ListLink>
         </li>
         <li>
-          <a href="#">Computadores</a>
+          <ListLink href="#">
+            <img src={iconPc} alt="icone de computador" />
+            Computadores
+          </ListLink>
         </li>
         <li>
-          <a href="#">CelListares</a>
+          <ListLink href="#">
+            <img src={iconSmartphone} alt="icone de oferta" />
+            Celulares
+          </ListLink>
         </li>
         <li>
-          <a href="#">Aparelhos de som</a>
+          <ListLink href="#">
+            <img src={iconSound} alt="icone de oferta" />
+            Aparelhos de som
+          </ListLink>
         </li>
       </List>
-    </div>
+      </div>
+    </CategoriesContainer>
   )
 }
 
--- src/components/CategoriesBar/styles.js
+++ src/components/CategoriesBar/styles.js
@@ -1,6 +1,32 @@
 import styled from "styled-components"
 
+export const CategoriesContainer = styled.div`
+  border-bottom: 1px solid #4B4B4D;
+`
+
 export const List = styled.ul`
   display: flex;
   justify-content: space-between;
+`
+
+export const ListLink = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #222;
+  padding: 24px;
+  width: 140px;
+  /* box-shadow: 0 0 5px rgba(0, 0, 0, 0.1); */
+  transition: box-shadow 0.3s ease-in-out;
+
+
+    &:hover {
+      box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+      border-radius: 10px
+    }
+
+  img {
+    max-width: 40px;
+    margin-bottom: 8px;
+  }
 `
